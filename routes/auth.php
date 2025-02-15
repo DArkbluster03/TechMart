ba52f1cb4b95961f4d7ed566a6be7966c18d27ea
Reza Alvi <alvireza24@gmail.com>
2025-02-16 02:20:29 +0600

    Register

--- routes/auth.php
+++ routes/auth.php
@@ -2,8 +2,15 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
 Route::middleware('guest')->group(function () {
+
+    Route::get('register', [RegisteredUserController::class, 'create'])
+    ->name('register');
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
