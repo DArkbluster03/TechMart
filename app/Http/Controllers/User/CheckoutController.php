4b5cf7b679f64427570fc694884cfb8bfd4601f9
DArkbluster03 <darkbluster03@gmail.com>
2025-03-09 08:43:38 +0600

    checkout remaining

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\User;
+
+use App\Helper\Cart;
 use App\Http\Controllers\Controller;
+use App\Models\CartItem;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Payment;
 use App\Models\UserAddress;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Inertia\Inertia;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CheckoutController extends Controller
 {
@@ -69,6 +76,71 @@ class CheckoutController extends Controller
             $address->type = $newAddress['type'];
             $address->user_id = Auth::user()->id;
             $address->save();
+        }
+        $mainAddress = $user->user_address()->where('isMain', 1)->first();
+        if ($mainAddress) {
+            $order = new Order();
+            $order->status = 'unpaid';
+            $order->total_price = $request->total;
+            $order->session_id = $checkout_session->id;
+            $order->created_by = $user->id;
+            // If a main address with isMain = 1 exists, set its id as customer_address_id
+            $order->user_address_id = $mainAddress->id;
+            $order->save();
+            $cartItems = CartItem::where(['user_id' => $user->id])->get();
+            foreach ($cartItems as $cartItem) {
+                OrderItem::create([
+                    'order_id' => $order->id, // Assuming you have an 'id' field in your orders table
+                    'product_id' => $cartItem->product_id,
+                    'quantity' => $cartItem->quantity,
+                    'unit_price' => $cartItem->product->price, // You may adjust this depending on your logic
+                ]);
+                $cartItem->delete();
+                //remove cart items from cookies
+                $cartItems = Cart::getCookieCartItems();
+                foreach ($cartItems as $item) {
+                    unset($item);
+                }
+                array_splice($cartItems, 0, count($cartItems));
+                Cart::setCookieCartItems($cartItems);
+            }
+
+            $paymentData = [
+                'order_id' => $order->id,
+                'amount' => $request->total,
+                'status' => 'pending',
+                'type' => 'stripe',
+                'created_by' => $user->id,
+                'updated_by' => $user->id,
+                // 'session_id' => $session->id
+            ];
+
+            Payment::create($paymentData);
+        }
+        return Inertia::location($checkout_session->url);
+    }
+
+    public function success(Request $request)
+    {
+        \Stripe\Stripe::setApiKey(env('STRIPE_KEY'));
+        $sessionId = $request->get('session_id');
+        try {
+            $session = \Stripe\Checkout\Session::retrieve($sessionId);
+            if (!$session) {
+                throw new NotFoundHttpException;
+            }
+            $order = Order::where('session_id', $session->id)->first();
+            if (!$order) {
+                throw new NotFoundHttpException();
+            }
+            if ($order->status === 'unpaid') {
+                $order->status = 'paid';
+                $order->save();
+            }
+
+            return redirect()->route('dashboard');
+        } catch (\Exception $e) {
+            throw new NotFoundHttpException();
         }
     }
 }
